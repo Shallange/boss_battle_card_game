70722eba7a188c481736810af5422249629f2b61
Shallange <jimmy.kroneld@hotmail.com>
2025-11-21 17:50:44 +0100

    Add Main menu layout, fonts and strings

--- app/src/main/java/com/example/bossbattlecardgame/MainMenuActivity.kt
+++ app/src/main/java/com/example/bossbattlecardgame/MainMenuActivity.kt
@@ -3,11 +3,11 @@ package com.example.bossbattlecardgame
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_menu)
 
     }
 }
