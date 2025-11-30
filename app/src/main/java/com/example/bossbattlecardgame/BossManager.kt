7a82b809f023d6bdd2beeffc19f997f34e4583de
Shallange <jimmy.kroneld@hotmail.com>
2025-11-30 20:14:26 +0100

    Move boss creation to Bossmanager from GameActivity

--- app/src/main/java/com/example/bossbattlecardgame/BossManager.kt
+++ app/src/main/java/com/example/bossbattlecardgame/BossManager.kt
@@ -6,7 +6,16 @@ class BossManager {
     init {
         initData()
     }
-    private fun initData() {
 
+    private fun initData() {
+        bosses.add(
+            Boss("Infernal Dread knight",340,R.drawable.infernaldreadknight)
+        )
+        bosses.add(
+            Boss("Emberlord Malakar",340,R.drawable.emberlordmalakar),
+        )
+        bosses.add(
+            Boss("Shadow Ignaroth",340, R.drawable.shadowignaroth)
+        )
     }
 }
