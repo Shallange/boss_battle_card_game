975db5c3f6812291f8a4c354830796c074bb2883
Shallange <jimmy.kroneld@hotmail.com>
2025-11-30 19:41:32 +0100

    Add bossmanager file with basic structure

---
+++ app/src/main/java/com/example/bossbattlecardgame/BossManager.kt
@@ -0,0 +1,12 @@
+package com.example.bossbattlecardgame
+
+class BossManager {
+
+    val bosses = mutableListOf<Boss>()
+    init {
+        initData()
+    }
+    private fun initData() {
+
+    }
+}
