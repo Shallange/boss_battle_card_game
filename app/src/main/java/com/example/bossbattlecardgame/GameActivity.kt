b2e8a57a38bb936c3f94e7fadbd3cedfb213d116
Shallange <jimmy.kroneld@hotmail.com>
2025-12-02 16:41:49 +0100

    Connect GameActivity with viewmodel and load first boss

--- app/src/main/java/com/example/bossbattlecardgame/GameActivity.kt
+++ app/src/main/java/com/example/bossbattlecardgame/GameActivity.kt
@@ -2,12 +2,18 @@ package com.example.bossbattlecardgame
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class GameActivity : AppCompatActivity() {
+    private lateinit var viewModel: GameViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        viewModel = ViewModelProvider(this)[GameViewModel::class.java]
+
+        viewModel.loadBoss(1)
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainer, BossFragment())
--- app/src/main/java/com/example/bossbattlecardgame/GameViewModel.kt
+++ app/src/main/java/com/example/bossbattlecardgame/GameViewModel.kt
@@ -1,8 +1,17 @@
 package com.example.bossbattlecardgame
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    private val BossManager = BossManager();
+    private val bossManager = BossManager()
+    private val _currentBoss = MutableLiveData<Boss>()
 
+    val currentBoss: LiveData<Boss> = _currentBoss
+
+    fun loadBoss(id: Int) {
+        val boss = bossManager.getBoss(id) ?: return
+        _currentBoss.value = boss
+    }
 }
