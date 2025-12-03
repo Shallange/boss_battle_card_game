db0011141d0c05505f91517c4f84b4e0fb368244
Shallange <jimmy.kroneld@hotmail.com>
2025-12-03 16:14:18 +0100

    Add onViewCreated setup in bossfragment and playerfragment

--- app/src/main/java/com/example/bossbattlecardgame/BossFragment.kt
+++ app/src/main/java/com/example/bossbattlecardgame/BossFragment.kt
@@ -1,5 +1,27 @@
 package com.example.bossbattlecardgame
 
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 
-class BossFragment : Fragment(R.layout.fragment_boss) {}
+class BossFragment : Fragment(R.layout.fragment_boss) {
+
+    private lateinit var viewModel: GameViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity())[GameViewModel::class.java]
+
+        val nameView = view.findViewById<TextView>(R.id.textBossName)
+        val imageView = view.findViewById<ImageView>(R.id.imgBoss)
+
+        viewModel.currentBoss.observe(viewLifecycleOwner) { boss ->
+            nameView.text = boss.name
+            imageView.setImageResource(boss.imageResId)
+        }
+    }
+
+}
--- app/src/main/java/com/example/bossbattlecardgame/PlayerFragment.kt
+++ app/src/main/java/com/example/bossbattlecardgame/PlayerFragment.kt
@@ -1,5 +1,17 @@
 package com.example.bossbattlecardgame
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 
-class PlayerFragment : Fragment(R.layout.fragment_player) {}
+class PlayerFragment : Fragment(R.layout.fragment_player) {
+    private lateinit var viewModel: GameViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity())[GameViewModel::class.java]
+
+    }
+}
